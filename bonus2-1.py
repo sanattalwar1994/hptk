0de0f44b9eab1a6ce9220de38a7a7fbb26da883a
Mr.Talwar <sanat@cogoport.com>
2019-08-20 16:11:33 +0530

    Lambda function added

--- bonus2-1.py
+++ bonus2-1.py
@@ -4,7 +4,7 @@ import boto3
 def lambda_handler(event, context):
     lb = boto3.client('elbv2')
     ec2 = boto3.client('ec2', region_name=ap-south-1)
-    lb1 = lb.describe_target_health(TargetGroupArn='arn:aws:elasticloadbalancing:ap-south-1:366169947947:targetgroup/RailsWebServerTG/43d9eb0df5f24d50')
+    lb1 = lb.describe_target_health(TargetGroupArn='')
     idlist = []
     for bals in lb1['TargetHealthDescriptions']:
       idlist.append(bals['Target']['Id'])
